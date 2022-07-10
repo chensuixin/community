5a2d23e70c3d4eef9d1418b03770a216a861a61f
chensuixin <1060148945@qq.com>
2022-07-10 22:45:10 +0800

    'Test1'

---
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -0,0 +1,47 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.*;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model, Page page) {
+        // 方法调用之前 springMVC会自动实例化Model和Page，并将Page注入到Model
+        // 所以在thymeleaf就可以访问Page
+        page.setRows(discussPostService.findDiscussRows(0));
+        page.setPath("/index");
+
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(list)) {
+            for (DiscussPost post : list) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("post",post);
+                User user = userService.findUserById(post.getUserId());
+                map.put("user",user);
+                discussPosts.add(map);
+            }
+        }
+        model.addAttribute("discussPosts",discussPosts);
+        return "/index";
+    }
+}
---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,17 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId,int offset,int limit);
+
+    //动态参数有且只有一个的时候  强制使用@Param
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
